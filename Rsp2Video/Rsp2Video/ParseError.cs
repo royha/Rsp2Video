b074ed9751dadba426351b676161eebde2df6be9
Roy Harper <royha99@gmail.com>
2023-08-06 14:13:59 -0700

    Add error reporting and correcting to bok/FmBok file parsing.

---
+++ Rsp2Video/Rsp2Video/ParseError.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rsp2Video
+{
+    public partial class ParseError : Form
+    {
+        public ParseError()
+        {
+            InitializeComponent();
+        }
+
+        public ParseError(String ParseErrorString)
+        {
+            InitializeComponent();
+            textBox1.Text = ParseErrorString;
+        }
+    }
+}
